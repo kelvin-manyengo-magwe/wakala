d7e856dfb3ba0a938e8e68276f42e1f8df24cd69
kelvin magwe <kelvinmullar5@gmail.com>
2024-10-23 12:08:51 +0300

    Complete write BackgroundRunning.tsx

---
+++ wakala_app/BackgroundRunning/BackgroundRunning.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import BackgroundFetch from 'react-native-background-fetch';
+import { ReadSms } from '../SmsCatcher/ReadSms';
+
+
+const BackgroundRunning = () => {
+
+        useEffect(() => {
+                 //configuring background fetch
+                 BackgroundFetch.configure({
+                    minimumFetchInterval: 1,  //Fetch every 1 minutes
+                    stopOnTerminate: false,  //continue even after app is closed
+                    startOnBoot: true,
+                 },  async(taskId) => {
+                            console.log(`[BackgroundFetch] taskId: `, taskId);
+
+                            //performing the background tasks
+                                ReadSms();
+
+                            //calling the finish method when task is done
+                                BackgroundFetch.finish(taskId);
+                 }, (error) => {
+                        console.log(`[BackgroundFetch] failed to start`, error);
+                 });
+
+                        //starting the background fetch
+                            BackgroundFetch.start();
+
+                            return () => {
+                                    //stopping background fetch when component unmounts
+                                    BackgroundFetch.stop();
+                            }
+        },[]);
+
+        return null;
+}
+
+export default BackgroundRunning;
