3758bc371297fe3144b9563b13f291e485394f5b
kelvin magwe <kelvinmullar5@gmail.com>
2024-11-19 02:35:04 +0300

    Added debuggers to sms background task

--- wakala_app/BackgroundRunning/BackgroundRunning.tsx
+++ wakala_app/BackgroundRunning/BackgroundRunning.tsx
@@ -2,45 +2,64 @@ import React, { useState, useEffect } from 'react';
 import BackgroundService from 'react-native-background-actions';
 import { ReadSms } from '../SmsCatcher/ReadSms';
 
-
+        const debugLog = (message) => {
+                console.log(`[DEBUG] ${message}`);
+        }
 const BackgroundRunning = (): null => {
 
-       const sleep = (time) => { // function to make the sleep success of promise periodically for long running background task
-                new Promise((resolve) => setTimeout(()=> resolve(),time)
-       } ; //the only success part of the promise
+        debugLog('Background Running component mounted');
+
+            // function to make the sleep success of promise periodically for long running background task
+       const sleep = (time) => new Promise((resolve) => setTimeout(()=> resolve(),time)); //the only success part of the promise
+
 
         const veryIntenseTask = async(taskDataArguments) => {
                     const { delay } = taskDataArguments;
 
-                    await new Promise( async(resolve) => {
-                        for(i= 0; BackgroundService.isRunning(); i++) {
-                                console.log(i);
-                                ReadSms();
-                                await sleep(delay);
-                        }
-                    });
+                        debugLog(`Starting very intense task with: ` + delay);
+
+                            console.log(BackgroundService.isRunning(),delay);
+
+                            try {
+                                        for(i= 0; BackgroundService.isRunning(); i++) {
+                                                                        console.log(i);
+
+                                                                      debugLog(`Readsms component running in background`);
+
+                                                                        ReadSms();
+                                                                        await sleep(delay);
+                                                                }
+
+
+                                                            const options = {
+                                                                        taskName: 'Sms BackgroundService',
+                                                                        taskTitle: 'Wakala Background service',
+                                                                        taskDesc: 'Running Background sms service',
+                                                                        taskIcon: {
+                                                                            name: 'ic_launcher',
+                                                                            type: 'mipmap',
+                                                                        },
+                                                                        color: '#ff00ff',
+                                                                        linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+                                                                        parameters: {
+                                                                            delay: 1000,
+                                                                        },
+                                                                    };
+
+                                                    debugLog(`Attemping to start the background service.`);
+                                                 await BackgroundService.start(veryIntensiveTask, options);
+                                                    debugLog(`BackgroundService started successfully`);
+
+                                                 await BackgroundService.updateNotification({taskDesc: 'New ExampleTask description'}); // Only Android, iOS will ignore this call
+                                                        debugLog(`Backgroundservice notification updated successfully`);
+                                                                    // iOS will also run everything here in the background until .stop() is called
+                            } catch(error) {
+                                    debugLog(`Error starting background service: ${error}`);
+                            }
+
         };
 
-        const options = {
-            taskName: 'Sms BackgroundService',
-            taskTitle: 'Wakala Background service',
-            taskDesc: 'Running Background sms service',
-            taskIcon: {
-                name: 'ic_launcher',
-                type: 'mipmap',
-            },
-            color: '#ff00ff',
-            linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
-            parameters: {
-                delay: 1000,
-            },
-        };
 
-
-        await BackgroundService.start(veryIntensiveTask, options);
-        await BackgroundService.updateNotification({taskDesc: 'New ExampleTask description'}); // Only Android, iOS will ignore this call
-        // iOS will also run everything here in the background until .stop() is called
-        await BackgroundService.stop();
 
 
         return null;
