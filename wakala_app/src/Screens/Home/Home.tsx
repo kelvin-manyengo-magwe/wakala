5b003938971c9c4465add3dc55c4f5770367ba55
kelvin magwe <kelvinmullar5@gmail.com>
2025-03-26 17:16:53 +0300

    Added greeting message to wakala at Home View

--- wakala_app/src/Screens/Home/Home.tsx
+++ wakala_app/src/Screens/Home/Home.tsx
@@ -7,38 +7,48 @@ export const Home = () => {
 
 
     return (
-            <>
-                 <View style={styles.header}>
-                          <View>
-                                  <Text style={styles.wakalaFont}>Wakala</Text>
-                          </View>
+            <View style={styles.whiteBackground}>
+                     <View style={styles.header}>
+                              <View>
+                                      <Text style={styles.wakalaFont}>Wakala</Text>
+                              </View>
 
-                          <View style={styles.iconContainer}>
-                                   <TouchableOpacity style={styles.iconSpacing}>
-                                            <Ionicons name="notifications-outline" size={20} />
-                                   </TouchableOpacity>
+                              <View style={styles.iconContainer}>
+                                       <TouchableOpacity style={styles.iconSpacing}>
+                                                <Ionicons name="notifications-outline" size={20} />
+                                       </TouchableOpacity>
 
-                                   <TouchableOpacity style={styles.iconSpacing}>
-                                            <Ionicons name="person-outline" size={20} />
-                                   </TouchableOpacity>
-                          </View>
-                 </View>
-            </>
+                                       <TouchableOpacity style={styles.iconSpacing}>
+                                                <Ionicons name="person-outline" size={20} />
+                                       </TouchableOpacity>
+                              </View>
+                     </View>
+
+                     <View style={styles.welcomeMessage}>
+                            <Text>Habari za jioni, Magwe</Text>
+                     </View>
+
+            </View>
     )
 };
 
 
 
 const styles = StyleSheet.create({
+       whiteBackground : {
+            flex: 1,
+            backgroundColor: 'white'
+       },
        header: {
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
-            elevation: 4,
-            shadowColor: '#000',
+            /*elevation: 4,
+            shadowRadius: 4,
+            shadowColor: 'black',
             shadowOpacity: 0.1,
-            shadowOffset: { width: 0, height: 2 },
-            margin: 5
+            shadowOffset: { width: 0, height: 2 },*/
+            paddingHorizontal: 10
        },
        iconContainer: {
             flexDirection: 'row',
@@ -52,5 +62,9 @@ const styles = StyleSheet.create({
             fontWeight: 'bold',
             fontSize: 20,
             color: 'black'
+       },
+       welcomeMessage: {
+            marginVertical: 10,
+            marginHorizontal: 10
        }
 });
