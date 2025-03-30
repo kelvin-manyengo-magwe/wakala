cbd43f9d4a866f796a610b64722958778e5de02f
kelvin magwe <kelvinmullar5@gmail.com>
2025-03-30 17:41:59 +0300

    Inserted the deposi left row at the components/TotalSummary/TotalSummary.tsx component

--- wakala_app/src/components/TotalSummary/TotalSummary.tsx
+++ wakala_app/src/components/TotalSummary/TotalSummary.tsx
@@ -13,7 +13,29 @@ export const totalSummaryWidth = width * 0.9;
 
             return (
                     <View style={styles.summaryContainer}>
+                            <View style={styles.leftSide}>
+                                    <View style={styles.infoContainer}>
+                                            <FontAwesome name="bank" size={20} />
 
+                                            <View style={styles.infoColumn}>
+                                                   <Text style={styles.label}>Deposits</Text>
+                                                   <Text style={styles.amount}>120,000 Tzshs</Text>
+                                            </View>
+                                    </View>
+
+                                    <View style={styles.infoContainer}>
+                                            <FontAwesome name="percent" size={20} />
+
+                                            <View style={styles.infoColumn}>
+                                                   <Text style={styles.label}>Kamisheni</Text>
+                                                   <Text style={styles.amount}>20,000 Tzshs</Text>
+                                            </View>
+                                    </View>
+                            </View>
+
+                            <View style={styles.rightSide}>
+
+                            </View>
                     </View>
 
             )
--- wakala_app/src/components/TotalSummary/styles.ts
+++ wakala_app/src/components/TotalSummary/styles.ts
@@ -3,13 +3,39 @@ import { totalSummaryHeight, totalSummaryWidth, width } from './TotalSummary';
 
 
 export const styles = StyleSheet.create({
-
     summaryContainer: {
         width: totalSummaryWidth, // 90% of screen width
         height: totalSummaryHeight, // 20% of screen height
         borderWidth: 1,
         borderColor: 'rgba(0,0,0,0.3)', // Add border color for visibility
         borderRadius: 10, // Rounded corners for better aesthetics
-
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    leftSide: {
+        flex: 1,  //to take equal space
+    },
+    rightSide: {
+        flex: 1,    //to take equal space
+    },
+    infoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    amount: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#007bff"
+    },
+    infoColumn: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
     },
 });
