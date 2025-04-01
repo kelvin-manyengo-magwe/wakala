87845fb3e7fd81cda09190d7d4244ba48d5ec04a
kelvin magwe <kelvinmullar5@gmail.com>
2025-04-01 08:20:34 +0300

    Put scrollView component at /Screens/Home/Home

--- wakala_app/src/Screens/Home/Home.tsx
+++ wakala_app/src/Screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, Text, StyleSheet, View, StatusBar } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, View, StatusBar, ScrollView } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { styles } from './styles';
 import { SlidingCard } from '../../components/SlidingCard/SlidingCard';
@@ -13,44 +13,44 @@ export const Home = () => {
 
 
     return (
+        <ScrollView>
+                    <View style={styles.whiteBackground}>
+                             <View style={styles.header}>
+                                      <View>
+                                              <Text style={styles.wakalaFont}>Wakala</Text>
+                                      </View>
 
-            <View style={styles.whiteBackground}>
-                     <View style={styles.header}>
-                              <View>
-                                      <Text style={styles.wakalaFont}>Wakala</Text>
-                              </View>
+                                      <View style={styles.iconContainer}>
+                                               <TouchableOpacity style={styles.iconSpacing}>
+                                                        <Ionicons name="notifications-outline" size={20} />
+                                               </TouchableOpacity>
 
-                              <View style={styles.iconContainer}>
-                                       <TouchableOpacity style={styles.iconSpacing}>
-                                                <Ionicons name="notifications-outline" size={20} />
-                                       </TouchableOpacity>
+                                               <TouchableOpacity style={styles.iconSpacing}>
+                                                        <Ionicons name="person-outline" size={20} />
+                                               </TouchableOpacity>
+                                      </View>
+                             </View>
 
-                                       <TouchableOpacity style={styles.iconSpacing}>
-                                                <Ionicons name="person-outline" size={20} />
-                                       </TouchableOpacity>
-                              </View>
-                     </View>
-
-                     <View style={styles.greetingContainer}>
-                            <Text>Habari za jioni, Magwe</Text>
-                     </View>
+                             <View style={styles.greetingContainer}>
+                                    <Text>Habari za jioni, Magwe</Text>
+                             </View>
 
 
-                     <View style={styles.contentContainers}>
-                                <View style={styles.slidingCardWrapper}>
-                                        <SlidingCard cardData={CardData} />
-                                </View>
+                             <View style={styles.contentContainers}>
+                                        <View style={styles.slidingCardWrapper}>
+                                                <SlidingCard cardData={CardData} />
+                                        </View>
 
-                                 <View style={styles.summaryParentContainer}>
-                                        <TotalSummary />
-                                 </View>
+                                         <View style={styles.summaryParentContainer}>
+                                                <TotalSummary />
+                                         </View>
 
-                                 <View style={styles.depositsWithdrawal}>
-                                         <DepositsWithdrawalBarChart />
-                                 </View>
-                     </View>
-            </View>
-
+                                         <View style={styles.depositsWithdrawal}>
+                                                 <DepositsWithdrawalBarChart />
+                                         </View>
+                             </View>
+                    </View>
+        </ScrollView>
     )
 };
 
