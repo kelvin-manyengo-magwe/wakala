025919634d06e740edd17f345702b63f097c3bdc
kelvin magwe <kelvinmullar5@gmail.com>
2025-04-03 11:48:25 +0300

    Inserted Images and border (ingest quality for user to be attempted to click at the mno on Home View

--- wakala_app/src/components/MnoServicePanel/MnoServicePanel.tsx
+++ wakala_app/src/components/MnoServicePanel/MnoServicePanel.tsx
@@ -1,12 +1,40 @@
-import { Text, View } from 'react-native';
-
+import { Text, View, Image } from 'react-native';
+import { styles } from './styles';
 
 
 export const MnoServicePanel = () => {
 
         return (
                 <View>
-                        <Text>Orodha ya mitandao</Text>
+                        <Text style={styles.headerText}>Orodha ya mitandao</Text>
+
+                        <View style={styles.mnoContainer}>
+                                    <View style={styles.mnoUpperRow}>
+                                            <View style={styles.mnoLogoContainer}>
+                                                    <Image resizeMode="cover" style={styles.mnoLogo} source={ require('../../../assets/images/icons/mpesa-logo.jpg') } />
+                                            </View>
+
+                                            <View style={styles.mnoLogoContainer}>
+                                                     <Image resizeMode="cover" style={styles.mnoLogo} source={ require('../../../assets/images/icons/halo-pesa-logo.png') } />
+                                            </View>
+
+                                            <View style={styles.mnoLogoContainer}>
+                                                     <Image resizeMode="cover" style={styles.mnoLogo} source={ require('../../../assets/images/icons/mixx-by-yas-logo.png') } />
+                                            </View>
+                                    </View>
+
+
+
+                                    <View style={styles.mnoBottomRow}>
+                                            <View style={styles.mnoLogoContainer}>
+                                                    <Image resizeMode="cover" style={styles.mnoLogo} source={ require('../../../assets/images/icons/airtel-money-logo.png') } />
+                                            </View>
+
+                                            <View style={styles.mnoLogoContainer}>
+                                                   <Image resizeMode="contain" style={styles.mnoLogo} source={ require('../../../assets/images/icons/t-pesa-logo.jpg') } />
+                                            </View>
+                                    </View>
+                        </View>
                 </View>
             )
     };
---
+++ wakala_app/src/components/MnoServicePanel/styles.ts
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+        headerText: {
+            fontSize: 18,
+            fontWeight: 'bold',
+            padding: 5,
+            color: 'black',
+        },
+        mnoContainer: {
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+        },
+        mnoUpperRow: {
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            paddingBottom: 10,
+        },
+        mnoBottomRow: {
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+        },
+        mnoLogo: {
+            width: 100,
+            height: 85,
+        },
+        mnoLogoContainer: {
+            marginHorizontal: 10,
+            borderWidth: 1,
+            borderColor: 'rgba(0,0,0,0.1)',
+            padding: 1,
+            borderRadius: 10,
+        },
+    });
