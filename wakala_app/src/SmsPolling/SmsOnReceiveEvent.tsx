c7e39d510a74255af962065b6c42463c26fb7e0f
kelvin magwe <kelvinmullar5@gmail.com>
2025-04-24 23:10:15 +0300

    Adding pouchDB database for the storage

--- wakala_app/src/SmsPolling/SmsOnReceiveEvent.tsx
+++ wakala_app/src/SmsPolling/SmsOnReceiveEvent.tsx
@@ -16,12 +16,15 @@ export const SmsOnReceiveEvent = () => {
                                                                         console.log('New sms Received: ',sms);
                                                 });
 
-                                         return () => SmsReceiverEventEmitter.removeAllListeners('sms_onReceive');
+                                         return () => SmsRe+ceiverEventEmitter.removeAllListeners('sms_onReceive');
                         } else {
                             console.warn("SmsReceiverModule is not available.");
+
                         }
 
             },[]);
 
                     return null;
 }
+
+
